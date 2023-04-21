934d9f95187ef2df37b7d13472a2466d1a920833
Stryders7 <fa.santibanez@duocuc.cl>
2023-04-20 21:11:38 -0400

    b2 con js

---
+++ js/app.js
@@ -0,0 +1,17 @@
+console.log("Estamos conectados");
+document.write("<h1>Mensaje de alerta desde js</h1>")
+//alert("Danger.......")
+
+var nt1 = 4.5;
+var nt2 = 2.0;
+var nt3 = 7.0;
+var sum = nt1 + nt2 + nt3;
+var prom = sum / 3;
+console.log("La suma de sus notas es: ", sum);
+console.log("El promedio es", prom);
+if(prom >= 4){
+    console.log("Usted aprobo el ramo")
+}
+else {
+    console.log("Usted no aprobo el ramo")
+}
